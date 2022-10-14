caadece6fd44605d1f020c521ae22592cc6e9f9f
Dev Redoy <mdredoyislam84@gmail.com>
2022-10-14 21:52:41 +0600

    Theme Format

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package meghnaagrofeed
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'meghnaagrofeed' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'meghnaagrofeed' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'meghnaagrofeed' ), 'meghnaagrofeed', '<a href="https://redoyislam.xyz/">MD REDOY ISLAM</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
